ee3808a6a6725e98aa5d6218bbdf4b4b08342c79
Shaan Thakker <shaanthakker@Shaans-MBP.fios-router.home>
2024-07-08 13:42:16 -0400

    fix ui bug for view page

--- app/components/ViewQuizPage/ViewingName.tsx
+++ app/components/ViewQuizPage/ViewingName.tsx
@@ -6,10 +6,11 @@ import React from "react";
 export default function ViewingName({ name }: { name: string }) {
   return (
     <motion.div
-      initial={{ y: -100, opacity: 0 }}
+      initial={{ y: 100, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
+      className="fixed left-[50%] z-50 bottom-5"
     >
-      <div className="fixed left-[50%] translate-x-[-50%] bg-zinc-800 py-2 px-4 w-full sm:w-[28rem] rounded-full shadow-lg flex flex-row items-center justify-between ">
+      <div className="bg-zinc-800 translate-x-[-50%] py-2 px-5 w-full sm:w-[28rem] rounded-full shadow-lg flex flex-row items-center justify-between ">
         <p>
           You are viewing this quiz as{" "}
           <span className="font-semibold">{name}</span>
